b459c9fa7db804fd25d8cc49bb53636d3118fac6
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-07-31 11:47:54 +0300

    JSON Try Catch Serializing

---
+++ W2D5-Exceptions& Json Serializing/JSON Try Catch Menu Program/JSON Try Catch Menu Program/Employee.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSON_Try_Catch_Menu_Program
+{
+	public class Employee:IComparable<Employee>
+	{
+		public int Employee_ID { get; set; }
+		public string Employee_Name { get; set; }
+
+		public double Salary { get; set; }
+
+		private int age;
+		public Employee()
+		{
+
+		}
+		public int Age
+		{
+			get { return age; }
+			set
+			{
+				// During deserialization, accept the value but mark it if invalid
+				if (value < 18 || value > 60)
+				{
+					Console.WriteLine($"Warning: Deserialized invalid age: {value}. Setting to 18.");
+					age = 18; // or choose a default fallback
+				}
+				else
+				{
+					age = value;
+				}
+			}
+		}
+		public Employee(int id, string name, double salary, int age)
+		{
+			Employee_ID = id;
+			Employee_Name = name;
+			Salary = salary;
+			Age = age;
+
+		}
+		public static Employee Search_Employee_By_ID(List<Employee> Emps)
+		{
+			Console.WriteLine("enter id of the Employee");
+			int id = int.Parse(Console.ReadLine());
+			foreach (var emp in Emps)
+			{
+				if (emp.Employee_ID == id)
+				{
+
+					Console.WriteLine($"{emp.Employee_ID}");
+					Console.WriteLine($"{emp.Employee_Name} ");
+					Console.WriteLine($"{emp.Age} ");
+					Console.WriteLine($"{emp.Salary}");
+					return emp;
+
+				}
+
+			}
+			Console.WriteLine("employee not found");
+			return null;
+		}
+		public static Employee Search_Employee_By_Name(List<Employee> Emps)
+		{
+			Console.WriteLine("enter  Name of Employee");
+			string name = Console.ReadLine();
+			foreach (var Emp in Emps)
+			{
+				if (Emp.Employee_Name == name)
+				{
+					Console.WriteLine($"{Emp.Employee_ID}");
+					Console.WriteLine($"{Emp.Employee_Name} ");
+					Console.WriteLine($"{Emp.Age} ");
+					Console.WriteLine($"{Emp.Salary}");
+					return Emp;
+				}
+			}
+			Console.WriteLine("Emp not found");
+			return null;
+		}
+		//public Employee Sort_ID(List<Employee> Emps):
+		//{
+		//}
+		public int CompareTo(Employee? other)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		//public void DisplayMethod(/*int id, string name, double salary, int Age*/)
+		//{
+		//	Console.WriteLine(" Employee Data ");
+		//	Console.WriteLine("ID: " + Employee_ID);
+		//	Console.WriteLine("Name: " + Employee_Name);
+		//	Console.WriteLine("Salary: " + Salary);
+		//	Console.WriteLine("Age" + Age);
+		//}
+	}
+
+	public class Sort_By_ID : IComparer<Employee>
+	{
+
+		public int Compare(Employee? x, Employee? y)
+		{
+			return x.Employee_ID.CompareTo(y.Employee_ID);
+		}
+	}
+	public class Sort_By_Name : IComparer<Employee>
+	{
+
+		public int Compare(Employee? x, Employee? y)
+		{
+			return x.Employee_Name.CompareTo(y.Employee_Name);
+		}
+	}
+	public class Sort_By_Salary : IComparer<Employee>
+	{
+
+		public int Compare(Employee? x, Employee? y)
+		{
+			return x.Salary.CompareTo(y.Salary);
+		}
+	}
+}
---
+++ W2D5-Exceptions& Json Serializing/JSON Try Catch Menu Program/JSON Try Catch Menu Program/Program.cs
@@ -0,0 +1,218 @@
+﻿using System.Text.Json;
+
+namespace JSON_Try_Catch_Menu_Program
+{
+	internal class Program
+	{
+		//public static Employee employee;
+		public static List<Employee> Employees = new List<Employee>();
+		static void Main(string[] args)
+		{
+			string[] Menu = { "New", "Display", "Exit", "Search_Emp-ByID", "Search_Emp-ByName", "Sort with ID ", "Sort by Name", "Sort by Salary", "Save", "Load" };
+			int digit = 0;
+			bool looping = true;
+			while (looping)
+			{
+				Console.Clear();
+				int xPosition = Console.WindowWidth / 2 - 10;
+				int yPosition = Console.WindowHeight / 4;
+				for (int i = 0; i < Menu.Length; i++)
+				{
+					if (i == digit)
+					{
+						Console.BackgroundColor = ConsoleColor.Blue;
+					}
+					else
+					{
+						Console.ForegroundColor = ConsoleColor.White;
+					}
+					Console.SetCursorPosition(xPosition, yPosition + i * 2);
+					Console.Write(Menu[i]);
+					Console.ResetColor();
+				}
+				ConsoleKeyInfo consoleKey = Console.ReadKey(true);
+				switch (consoleKey.Key)
+				{
+					case ConsoleKey.UpArrow:
+						if (digit == 0)
+						{
+							digit = Menu.Length - 1;
+						}
+						else
+						{
+							digit = digit - 1;
+						}
+						break;
+					case ConsoleKey.DownArrow:
+						if (digit == Menu.Length - 1)
+						{
+							digit = 0;
+						}
+						else
+						{
+							digit = digit + 1;
+						}
+						break;
+					case ConsoleKey.Enter:
+						Console.Clear();
+						switch (digit)
+						{
+							case 0:
+								NewMethod();
+								break;
+							case 1:
+								DisplayAllEmployees(Employees);
+								Console.ReadKey(true);
+								break;
+							case 2:
+								ExitMethod();
+								looping = false;
+								break;
+							case 3:
+								Employee.Search_Employee_By_ID(Employees);
+								Console.ReadKey();
+								break;
+							case 4:
+								Employee.Search_Employee_By_Name(Employees);
+								Console.ReadKey();
+								break;
+							case 5:
+								Employees.Sort(new Sort_By_ID());
+								Program.DisplayAllEmployees(Employees);
+								Console.ReadKey(true);
+								break;
+							case 6:
+								Employees.Sort(new Sort_By_Name());
+								Program.DisplayAllEmployees(Employees);
+								Console.ReadKey(true);
+								break;
+							case 7:
+								Employees.Sort(new Sort_By_Salary());
+								Program.DisplayAllEmployees(Employees);
+								Console.ReadKey(true);
+								break;
+							case 8:
+								JSON_Serialize_ListData();
+								Console.ReadKey();
+								break;
+							case 9:
+								JSONDeserialize();
+								Console.ReadKey();
+								break;
+							
+						}
+						break;
+				}
+			}
+		}
+		//int id = 0;
+		//string name = "";
+		//double salary = 0;
+		//int age = 0;
+		bool dataEntered = false;
+
+
+
+		public static void NewMethod(/*ref int id, ref string name, ref double salary, ref int age*/)
+		{
+			int id = 0;
+			string name = "";
+			double salary = 0;
+			int age = 0;
+			Console.WriteLine("Enter Employee Data ");
+
+			try
+			{
+				Console.Write("Enter Employee ID: ");
+				id = int.Parse(Console.ReadLine());
+				bool idExists = Employees.Any(emp => emp.Employee_ID == id);
+				if (idExists)
+				{
+					Console.WriteLine("❌ Employee ID already exists. Please enter a unique ID.");
+					return;
+				}
+
+				Console.Write("Enter Employee Name: ");
+				name = Console.ReadLine();
+
+				Console.Write("Enter Employee Salary: ");
+				salary = double.Parse(Console.ReadLine());
+
+				Console.Write("Enter Employee Age: ");
+				age = int.Parse(Console.ReadLine());
+
+				Employees.Add(new Employee(id, name, salary, age));
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($" error:{ex.Message}");
+			}
+			finally
+			{
+				Console.WriteLine("back to main screen");
+			}
+
+
+
+
+		}
+
+		public static void DisplayAllEmployees(List<Employee> Emps)
+		{
+			foreach (var Emp in Emps)
+			{
+				Console.WriteLine($"Employee ID= {Emp.Employee_ID}");
+				Console.WriteLine($"Employee name= {Emp.Employee_Name}");
+				Console.WriteLine($"Employee Salary= {Emp.Salary}");
+				Console.WriteLine($"Employee Age= {Emp.Age}");
+				Console.WriteLine("*************************************************************************");
+			}
+		}
+		static void ExitMethod()
+		{
+			Console.WriteLine("Exiting the program. Goodbye!");
+		}
+
+		private static void JSON_Serialize_ListData()
+		{
+			string JsonOutput = JsonSerializer.Serialize(Employees, new JsonSerializerOptions{
+				WriteIndented=true
+			});
+			Console.WriteLine(JsonOutput);
+			string filepath = "Employees.json";
+			File.WriteAllText(filepath, JsonOutput);
+
+		}
+
+		private static void JSONDeserialize()
+		{
+			string filepath = "Employees.json";
+			if (File.Exists(filepath))
+			{
+				string JSONinput = File.ReadAllText(filepath);
+				Employees = JsonSerializer.Deserialize<List<Employee>>(JSONinput);
+				if (Employees != null)
+				{
+					Console.WriteLine("deserialized data:");
+					foreach (var emp in Employees)
+					{
+						Console.WriteLine($"EmpID={emp.Employee_ID}");
+						Console.WriteLine($"EmpName={emp.Employee_Name}");
+						Console.WriteLine($"EmpSalary={emp.Salary}");
+						Console.WriteLine($"EmpAge={emp.Age}");
+						Console.WriteLine("*********************************");
+					}
+				}
+				else
+				{
+					Console.WriteLine("deserialization null");
+				}
+
+			}
+			else
+			{
+				Console.WriteLine("file not found");
+			}
+		}
+}	} 
+
